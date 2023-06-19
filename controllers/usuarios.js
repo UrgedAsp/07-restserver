f6091b514f3b68a572f0b116d38f3a1b458a22ad
Johann Castillo <jcastvas@emeal.nttdata.com>
2023-06-19 12:47:53 -0500

    CRUD

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,33 +1,52 @@
 const { response } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
-const usuariosGet = (req, res = response) => {
-  const { test, random } = req.query;
+const usuariosGet = async (req, res = response) => {
+  const { limite = 5, desde = 0 } = req.query;
+
+  const [total, usuarios] = await Promise.all([
+    Usuario.countDocuments({ estado: true }),
+    Usuario.find({ estado: true }).skip(Number(desde)).limit(Number(limite)),
+  ]);
   res.json({
-    message: 'get API - controlador',
-    test,
-    random,
+    total,
+    usuarios,
   });
 };
 
-const usuariosPut = (req, res) => {
-  const id = req.params.id;
-  res.json({
-    message: 'put API - controlador',
-    id,
-  });
+const usuariosPut = async (req, res) => {
+  const { id } = req.params;
+  const { _id, password, google, correo, ...resto } = req.body;
+  if (password) {
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    resto.password = bcryptjs.hashSync(password, salt);
+  }
+
+  const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+  res.json(usuario);
 };
-const usuariosPost = (req, res) => {
-  const { nombre, edad } = req.body;
-  res.json({
-    message: 'post API - controlador',
-    nombre,
-    edad,
-  });
+const usuariosPost = async (req, res) => {
+  const { nombre, correo, password, rol } = req.body;
+  const usuario = new Usuario({ nombre, correo, password, rol });
+
+  //Encriptar la contraseña
+  const salt = bcryptjs.genSaltSync();
+  usuario.password = bcryptjs.hashSync(password, salt);
+
+  //Guardar en BD
+  await usuario.save();
+  res.json(usuario);
 };
-const usuariosDelete = (req, res) => {
-  res.json({
-    message: 'delete API - controlador',
-  });
+const usuariosDelete = async (req, res) => {
+  const { id } = req.params;
+  // Borrado en Base de datos
+  // const usuario = await Usuario.findByIdAndDelete(id);
+
+  const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
+  res.json(usuario);
 };
 
 module.exports = {
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,54 @@
 const { Router } = require('express');
-const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete } = require('../controllers/usuarios');
+const {
+  usuariosGet,
+  usuariosPost,
+  usuariosPut,
+  usuariosDelete,
+} = require('../controllers/usuarios');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const {
+  esRoleValido,
+  emailExiste,
+  existeUsuarioPorId,
+} = require('../helpers/db-validators');
 
 const router = Router();
 
 router.get('/', usuariosGet);
-router.put('/:id', usuariosPut);
-router.post('/', usuariosPost);
-router.delete('/', usuariosDelete);
+router.put(
+  '/:id',
+  [
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    check('rol').custom(esRoleValido),
+    validarCampos,
+  ],
+  usuariosPut
+);
+router.post(
+  '/',
+  [
+    check('correo', 'El correo no es válido').isEmail(),
+    check('correo').custom(emailExiste),
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe de tener más de 6 caracteres').isLength(
+      { min: 6 }
+    ),
+    // check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(esRoleValido),
+    validarCampos,
+  ],
+  usuariosPost
+);
+router.delete(
+  '/:id',
+  [
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    validarCampos,
+  ],
+  usuariosDelete
+);
 
 module.exports = router;
