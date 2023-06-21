0aeec4dc6075ce68614c2a6a8a85ca28c3f688b5
Johann Castillo <jcastvas@emeal.nttdata.com>
2023-06-21 11:19:24 -0500

    google signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res) => {
   const { correo, password } = req.body;
@@ -32,8 +33,8 @@ const login = async (req, res) => {
     const token = await generarJWT(usuario.id);
 
     res.json({
-        usuario,
-        token
+      usuario,
+      token,
     });
   } catch (error) {
     console.log(error);
@@ -43,6 +44,46 @@ const login = async (req, res) => {
   }
 };
 
+const googleSingIn = async (req, res) => {
+  const { id_token } = req.body;
+  try {
+    const { nombre, img, correo } = await googleVerify(id_token);
+    let usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      const data = {
+        nombre,
+        correo,
+        password: ':P',
+        img,
+        rol: "USER_ROLE",
+        google: true,
+      };
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: 'Hable con el administrador, usuario bloqueado',
+      });
+    }
+
+    //Generar JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'El Token no se pudo verificar',
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSingIn,
 };
